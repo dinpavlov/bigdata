74a96ad0b05d78e00d65408a5f5a20804468bf1c
dinpavlov <dinpavlov@gmail.com>
2022-07-21 03:01:33 +0300

    fix task serializer

--- bigdata/bigdata/settings.py
+++ bigdata/bigdata/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-w%fws#dzd)&tfl71zj%qld!g+(_(q$7ga4tou-5&l-j88#le$3'
+SECRET_KEY = 'django-insecure-w%fws#dzd)&tfl71zj%qld!g+(_(q$7ga4tou-5&l-j88#le$3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = int(os.environ.get("DEBUG", default=0))
+#DEBUG = int(os.environ.get("DEBUG", default=1))
 
 ALLOWED_HOSTS = []
 
--- bigdata/tasks/serializers.py
+++ bigdata/tasks/serializers.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 class TaskSerializer(serializers.ModelSerializer):
     assigner_username = serializers.ReadOnlyField(source='assigner.username')
-    assigner = serializers.PrimaryKeyRelatedField(write_only=True, queryset=User.objects.all(), required=True)
     class Meta:
         model = Task
         fields = (
@@ -13,5 +12,4 @@ class TaskSerializer(serializers.ModelSerializer):
             'assigner_username',
             'task_name',
             'task_endtime',
-            'assigner',
         )
